61ea3538e00f0353f063ae8f8d7173577993c387
Frederic <jean-frederic.laprade@usherbrooke.ca>
2025-04-10 11:22:27 -0400

    Adding DPS to QuantumCrypto

---
+++ dps/routing.py
@@ -0,0 +1,14 @@
+from django.urls import re_path
+from dps.consumers import (PlayingRoomConsumer,
+                           WaitingRoomConsumer,
+                           ResultsPageConsumer, )
+
+websocket_urlpatterns = [
+    re_path(r'^ws/games/dps/(?P<game_code>\w+)/$',
+            WaitingRoomConsumer.as_asgi()),
+    re_path(r'^ws/games/dps/(?P<game_code>\w+)/rooms/(?P<room_id>\w+)/$',
+            PlayingRoomConsumer.as_asgi()),
+    re_path(r'^ws/games/dps/(?P<game_code>\w+)/results/$',
+            ResultsPageConsumer.as_asgi()),
+
+]
